dba4885cdb63cb8c563790cef039844897af7d73
apta-tamilarasi <athinarayanantamilarasi@gamil.com>
2023-10-28 16:11:38 +0530

    addnew

---
+++ main.js
@@ -0,0 +1,12 @@
+let a=prompt("Enter the number")
+
+let b=a.length
+
+console.log("Given number = "+a)
+console.log("Given number digit = "+b)
+
+document.write("given number ="+a+"<br>")
+document.write("Number digit  ="+b+"<br>")
+
+
+
